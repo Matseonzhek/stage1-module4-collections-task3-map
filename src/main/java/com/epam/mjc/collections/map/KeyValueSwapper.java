315242fd84237f28fae73224924a594b9e702920
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-09-14 10:06:06 +0400

    stage1 module4 task3 DONE

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,18 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class KeyValueSwapper {
+
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> result = new HashMap<>();
+        NavigableMap<Integer, String> navigableMap = new TreeMap<>(sourceMap).descendingMap();
+        for (Map.Entry<Integer, String> entry : navigableMap.entrySet()) {
+            result.put(entry.getValue(), entry.getKey());
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,27 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
+    public static void main(String[] args) {
+        Map<String, Integer> forPrinting = new WordRepetitionMapCreator().createWordRepetitionMap("");
+
+        for (Map.Entry<String, Integer> entrySet : forPrinting.entrySet()) {
+            System.out.println(entrySet.getKey() + " " + entrySet.getValue());
+        }
+    }
+
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        String[] splitSentence = sentence.split("[ .,]+");
+        Map<String, Integer> result = new HashMap<>();
+        for (String s : splitSentence) {
+            if (!result.containsKey(s.toLowerCase()) && sentence.length() > 0) {
+                result.put(s.toLowerCase(), 1);
+            } else {
+                result.computeIfPresent(s.toLowerCase(), (key, val) -> val = val + 1);
+            }
+        }
+        return result;
     }
 }
